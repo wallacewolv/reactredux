ab5e4fa64e9b6484a46977dd4d6371a55c4860f7
wallace <wallace_wolv@hotmail.com>
2024-10-16 00:22:10 -0300

    feat: Implementando uso do redux e uso do typescript para tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import "./App.css";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import reactLogo from "./assets/react.svg";
+import { increment, incrementAmount } from "./features/counter/counter-slice";
+import { useAppSelector } from "./hooks";
 import viteLogo from "/vite.svg";
 
-// function App({  count,increment, incrementAmount }) {
 function App() {
-  const count = useSelector((state) => state.counter.value);
+  const count = useAppSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
   function handleOnClick() {
-    // increment();
-    dispatch({ type: "counter/increment" });
+    dispatch(increment());
   }
 
   function handleOnClickAmount() {
-    // incrementAmount(5);
-    dispatch({ type: "counter/incrementAmount", payload: 5 });
+    dispatch(incrementAmount(5));
   }
 
   return (
@@ -50,20 +49,3 @@ function App() {
 }
 
 export default App;
-
-// OLD SCHOOL
-// const mapStateToProps = (state) => {
-//   return {
-//     count: state.counter.value,
-//   };
-// };
-
-// const mapDispatchProps = (dispatch) => {
-//   return {
-//     increment: () => dispatch({ type: "counter/increment" }),
-//     incrementAmount: (amount) =>
-//       dispatch({ type: "counter/incrementAmount", payload: amount }),
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchProps)(App);
